288b2d9d58d6e5e3fdd80effb58bc881e13b650c
Simon Hedberg <simon@in-formation.se>
2012-09-15 04:26:48 +0200

    small update to setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,5 @@ setup(
     packages=['mailgun_incoming'],
     url='http://github.com/hedberg/django-mailgun-incoming/',
     description='Django app for storing email and attached files received through the Mailgun API.',
-    long_description=open('README.rst').read(),
-    install_requires=[
-        "Django >= 1.3",
-    ],
+    long_description=open('README.rst').read()
 )
