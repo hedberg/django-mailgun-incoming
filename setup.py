585635b4af5cb94ac4c8de58b326e253933480c7
Ben Whalley <benwhalley@gmail.com>
2014-10-24 20:49:14 +0100

    bump v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-mailgun-incoming',
-    version='0.1dev',
+    version='0.1.1',
     author='Simon Hedberg',
     author_email='simon@in-formation.se',
     packages=['mailgun_incoming'],
