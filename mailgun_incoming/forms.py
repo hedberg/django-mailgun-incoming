f0f5f07b21848fe78f3e6203d4bd6eac3502a244
Ben Whalley <benwhalley@gmail.com>
2015-11-22 23:28:42 +0000

    neater solution to django 1.8 bug add test pep8

--- mailgun_incoming/forms.py
+++ mailgun_incoming/forms.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
-field_map = {'from_str':'from',
-             'body_plain':'body-plain',
-             'body_html':'body-html',
-             'stripped_text':'stripped-text',
-             'stripped_html':'stripped-html',
-             'message_headers':'message-headers',
-             'stripped_signature':'stripped-signature',
-             'content_id_map':'content-id-map'}
-
-
-class EmailForm(forms.ModelForm):    
+class EmailForm(forms.ModelForm):
     
-    def __init__(self, *args, **kwargs):
-        super(EmailForm, self).__init__(*args, **kwargs)
-        for (field_name, form_key) in field_map.items():
-            self.fields[form_key] = self.fields[field_name]
-            del self.fields[field_name]
-        self.fields['attachment-count'] = forms.IntegerField(required=False)
+    field_map = {
+        'body-html': 'body_html', 
+        'body-plain': 'body_plain', 
+        'content-id-map': 'content_id_map', 
+        'from': 'from_str', 
+        'message-headers': 'message_headers', 
+        'recipient': 'recipient', 
+        'sender': 'sender', 
+        'stripped-html': 'stripped_html', 
+        'stripped-signature': 'stripped_signature', 
+        'stripped-text': 'stripped_text', 
+        'subject': 'subject'
+    }
+
+    def __init__(self, mailgun_post=None, *args, **kwargs):
         
+        if not mailgun_post:
+            raise Exception("No mailgun post passed. Unbound form not supported.")
+
+        kwargs['data'] = {self.field_map.get(k, k): v for k, v in mailgun_post.items()}
+        super(EmailForm, self).__init__(*args, **kwargs)
+        self.fields['attachment-count'] = forms.IntegerField(required=False)
+
+    
--- mailgun_incoming/views.py
+++ mailgun_incoming/views.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
-import hashlib, hmac
+import hashlib
+import hmac
 import logging
+
 from django.conf import settings
-from django.http import HttpResponse, HttpResponseBadRequest
 from django.forms.models import modelform_factory
-from django.views.generic.base import View
-from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import View
+from mailgun_incoming.forms import EmailForm
 from mailgun_incoming.models import Attachment, IncomingEmail
 from mailgun_incoming.signals import email_received
-from mailgun_incoming.forms import EmailForm, field_map
 
 logger = logging.getLogger(__name__)
 
 API_KEY = getattr(settings, "MAILGUN_ACCESS_KEY", "")
-VERIFY_SIGNATURE = getattr(settings, "MAILGUN_VERIFY_INCOMING", API_KEY!="")
+
+VERIFY_SIGNATURE = getattr(settings, "MAILGUN_VERIFY_INCOMING", not settings.DEBUG)
+
 
 class Incoming(View):
     email_model = IncomingEmail
@@ -22,52 +26,56 @@ class Incoming(View):
     form = EmailForm
     api_key = API_KEY
     verify = VERIFY_SIGNATURE
-    
+
     def get_form(self):
         return modelform_factory(self.email_model, form=self.form, exclude=[])
-    
+
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(Incoming, self).dispatch(*args, **kwargs)
-    
+
     def post(self, request, *args, **kwargs):
+
         if self.verify:
-            verified = self.verify_signature(request.POST.get('token',''),
-                              request.POST.get('timestamp',''),
-                              request.POST.get('signature',''))
+            verified = self.verify_signature(request.POST.get('token', ''),
+                                             request.POST.get('timestamp', ''),
+                                             request.POST.get('signature', ''))
             if not verified:
-                logger.debug("Signature verification failed. Email posted from %s. %s" % (
-                              request.META.get('REMOTE_ADDR','<no remote addr>'),
-                              request.POST.get('subject', '')))
+                logger.debug("Signature verification failed.")
                 return HttpResponseBadRequest("Invalid signature")
-                
+
         form = self.get_form()(request.POST)
-        
-        email = form.save(commit=False)
-        # remap all the form fields to match the model fields, then save
-        for (field_name, form_key) in field_map.items():
-            if form.cleaned_data.get(form_key,None):
-                setattr(email,field_name,form.cleaned_data.get(form_key))
-        email.save()
+
+        email = form.save()
 
         attachments = []
-        if form.cleaned_data.get('attachment-count',0):
-            attachments = []
-            #reverse mapping in content_ids dict
-            content_ids = dict((attnr,cid) for cid,attnr in (email.content_ids or {}).iteritems())
-            i = 1
+        if form.cleaned_data.get('attachment-count', False):
+            
+            # reverse mapping in content_ids dict
+            content_ids = dict(
+                (attnr, cid) for cid, attnr in
+                (email.content_ids or {}).iteritems())
+            
             for file in request.FILES.values():
-                attachment = self.attachment_model(email=email, file=file, content_id=content_ids.get('attachment-{0!s}'.format(i),'')).save()
+                attachment = self.attachment_model(email=email, 
+                    file=file,  
+                    content_id=content_ids.get('attachment-{0!s}'.format(i), ''))
+                attachments.save()
+                
                 attachments.append(attachment)
-                i += 1
-        self.handle_email(email, attachments=attachments)
-        
-        return HttpResponse("OK")
-    
+
+        # See if any attached signal handlers throw an error
+        try:
+            self.handle_email(email, attachments=attachments)
+            return HttpResponse("OK")
+        except RejectedMailException, e:
+            return HttpResponse("Email not accepted", status=406)
+
     def handle_email(self, email, attachments=None):
-        email_received.send(sender=self.email_model, instance=email, attachments=attachments or [])
-    
+        email_received.send(
+            sender=self.email_model, instance=email, attachments=attachments or [])
+
     def verify_signature(self, token, timestamp, signature):
         return signature == hmac.new(key=self.api_key,
-                                 msg='{0}{1}'.format(timestamp, token),
-                                 digestmod=hashlib.sha256).hexdigest()
+                                     msg='{0}{1}'.format(timestamp, token),
+                                     digestmod=hashlib.sha256).hexdigest()
